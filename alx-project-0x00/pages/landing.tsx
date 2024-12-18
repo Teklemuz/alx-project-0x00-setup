247a61330bb95ff376d8bacdd093ac6d6ff7c83d
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-12-18 22:46:57 +0300

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,14 @@
-const Landing: React.FC =  () => {
-    return (
-      <div>
-        <h1 className=" text-xl font-extralight">Landing Page</h1>
-      </div>
-    )
-  }
-  export default Landing
+import Card from "@/components/Card";  
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+      <Card /> 
+      <Card />  
+      <Card />  
+    </div>
+  );
+}
+
+export default Landing;
