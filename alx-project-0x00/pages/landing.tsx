6589369733d83d21da1a8de7a015c3a3e0b57e5d
Teklemuz <111185891+Teklemuz@users.noreply.github.com>
2024-12-19 00:12:57 +0300

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,14 +1,27 @@
-import Card from "@/components/Card";  
+import React from "react";
+import Button from "../components/Button"; 
+import Card from "@/components/Card"; 
 
 const Landing: React.FC = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col items-center justify-center space-y-6">
       <h1 className="text-xl font-extralight">Landing Page</h1>
-      <Card /> 
-      <Card />  
-      <Card />  
+
+      {/* Button Instances */}
+      <div className="flex space-x-4">
+        <Button title="Small Button" styles="small" shape="rounded-sm" />
+        <Button title="Medium Button" styles="medium" shape="rounded-md" />
+        <Button title="Large Button" styles="large" shape="rounded-full" />
+      </div>
+
+      {/* Card Instances */}
+      <div className="mt-6 flex flex-col items-center space-y-4">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
-}
+};
 
 export default Landing;
